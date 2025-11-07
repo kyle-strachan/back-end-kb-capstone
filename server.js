b01f1d8264dd347389981ffe25d9637e55a1bbab
reiteach <kylestrachan@outlook.com>
2025-11-06 21:00:38 -0800

    Project init

---
+++ server.js
@@ -0,0 +1,6 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+
+// import { rateLimitMiddleware } // Add later
