e4d38fe6802f9088b294586aa50ef970e55c77ee
reiteach <kylestrachan@outlook.com>
2025-11-19 19:32:52 -0800

    Archive function now part of edit route

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -4,7 +4,6 @@ import {
   getDoc,
   newDoc,
   editDoc,
-  toggleArchiveDoc,
   getDocsTree,
 } from "../controllers/docsController.js";
 import { authMiddleware, attachUser } from "../middleware/authMiddleware.js";
@@ -17,13 +16,6 @@ router.get("/", authMiddleware, attachUser, getDocs); // Get all docs appropriat
 router.get("/tree", authMiddleware, attachUser, getDocsTree); // To output the explorer
 router.get("/:id", authMiddleware, attachUser, getDoc); // get single doc
 router.post("/", authMiddleware, attachUser, newDoc);
-router.patch("/:id", authMiddleware, attachUser, editDoc);
-// router.post("/:id/archive", authMiddleware, attachUser, toggleArchiveDoc(true));
-// router.post(
-//   "/:id/restore",
-//   authMiddleware,
-//   attachUser,
-//   toggleArchiveDoc(false)
-// );
+router.patch("/edit/:id", authMiddleware, attachUser, editDoc);
 
 export default router;
