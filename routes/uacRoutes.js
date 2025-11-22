d0b7da7a2029b880bdcdfdb9b9d505fb4a9e986d
reiteach <kylestrachan@outlook.com>
2025-11-22 13:28:08 -0800

    Dashboard support

--- routes/uacRoutes.js
+++ routes/uacRoutes.js
@@ -5,6 +5,9 @@ import {
   newAccessRequest,
   approveOrRejectRequest,
   revokeAccessRequest,
+  getToActionAccessRequests,
+  getMyAccessRequests,
+  getByAccessRequests,
 } from "../controllers/uacController.js";
 import { authMiddleware, attachUser } from "../middleware/authMiddleware.js";
 // import { noCache } from ...
@@ -27,6 +30,24 @@ router.post(
 
 // Requests
 router.get("/access-requests", authMiddleware, attachUser, getAccessRequests);
+router.get(
+  "/access-requests/to-action",
+  authMiddleware,
+  attachUser,
+  getToActionAccessRequests
+);
+router.get(
+  "/access-requests/my-requests",
+  authMiddleware,
+  attachUser,
+  getMyAccessRequests
+);
+router.get(
+  "/access-requests/by-requests",
+  authMiddleware,
+  attachUser,
+  getByAccessRequests
+);
 router.post("/access-requests", authMiddleware, attachUser, newAccessRequest);
 router.patch(
   "/access-requests/:id",
