9a9a2fa33ede3fd817f9a5cfaf355d7801dae267
reiteach <kylestrachan@outlook.com>
2025-11-23 13:03:55 -0800

    Doc search implemented

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -8,7 +8,7 @@ import {
   getDocsTree,
   uploadImage,
   signUrl,
-  // listDocImages,
+  getDocsSearch,
 } from "../controllers/docsController.js";
 import { authMiddleware, attachUser } from "../middleware/authMiddleware.js";
 // import { noCache } from ...
@@ -18,7 +18,7 @@ const router = Router();
 // All prefixed /api/docs
 router.get("/", authMiddleware, attachUser, getDocs); // Get all docs appropriate of users based on canViewAllDocs etc.
 router.get("/tree", authMiddleware, attachUser, getDocsTree); // To output the explorer
-// router.get("/:id/images", listDocImages);
+router.get("/search", authMiddleware, attachUser, getDocsSearch);
 router.get("/:id/sign-url", signUrl);
 router.get("/:id", authMiddleware, attachUser, getDoc); // get single doc
 
