fd491ec5e667ce24933a372ba4947af19544327a
reiteach <kylestrachan@outlook.com>
2025-12-01 22:07:13 -0800

    Consolidated from other files

--- utils/constants.js
+++ utils/constants.js
@@ -7,6 +7,14 @@ export const PASSWORD_MIN_LENGTH = 8;
 export const REGEX_USERNAME = /^[a-zA-Z0-9_]+$/;
 export const REGEX_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).+$/; // For submission, password complexity has been left light, use 12345678 if required.
 export const MINIMUM_LOCATION_LENGTH = 3;
+export const MINIMUM_DEPARTMENT_LENGTH = 3;
+export const MINIMUM_DEPARTMENT_CATEGORY_LENGTH = 3;
+export const MINIMUM_SYSTEM_LENGTH = 3;
+export const MINIMUM_SYSTEM_ADMINS = 1;
+export const MINIMUM_SYSTEM_CATEGORY_LENGTH = 3;
+export const MINIMUM_DOC_TITLE_LENGTH = 5;
+export const MINIMUM_DOC_DESCRIPTION_LENGTH = 10;
+export const MINIMUM_DOC_BODY_LENGTH = 20;
 
 // Cookies
 export const COOKIE_BASE_OPTIONS = {
