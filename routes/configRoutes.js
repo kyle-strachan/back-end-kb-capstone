96092293b82ba522928cf7a433c8c5796c9ec49b
reiteach <kylestrachan@outlook.com>
2025-12-06 18:41:34 -0800

    Remove permission required to see locations (required for new user drop downs)

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -40,7 +40,7 @@ router.get(
   "/locations",
   authMiddleware,
   attachUser,
-  requirePermission("locations.CanManage"),
+  // requirePermission("locations.CanManage"), // Removed, required to populate drop down
   getLocations
 ); // Get all locations
 router.post(
